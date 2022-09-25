bc2bb4feae14c075a3c9e28aca4eed26e6e6bb05
donovan2121 <gilchrist068@gmail.com>
2022-09-25 18:31:46 +0800

    edit registration route and template

--- app/forms.py
+++ app/forms.py
@@ -5,10 +5,10 @@ from wtforms.validators import DataRequired, Length
 class RegistrationForm(FlaskForm):
     reddit_username = StringField('Reddit Username', validators=[DataRequired()])
     course = StringField('Course/Industry', validators=[DataRequired()])
-    response_join = TextAreaField('Why do you want to join?', validators=[Length(min = 1, message = 'enter a minimum of 50 characters')])
-    response_goal = TextAreaField('What are your current Study Goals?', validators=[Length(min = 1, message = 'enter a minimum of 50 characters')])
-    response_pain = TextAreaField('What are your pain-points when Self-Studying?', validators=[Length(min = 1, message = 'enter a minimum of 50 characters')])
-    response_expect = TextAreaField('What do you expect from our Community?', validators=[Length(min = 1, message = 'enter a minimum of 50 characters')])
+    response_join = TextAreaField('Why do you want to join?', validators=[Length(min = 10, message = 'enter a minimum of 50 characters'), DataRequired()])
+    response_goal = TextAreaField('What are your current Study Goals?', validators=[Length(min = 10, message = 'enter a minimum of 50 characters'), DataRequired()])
+    response_pain = TextAreaField('What are your pain-points when Self-Studying?', validators=[Length(min = 10, message = 'enter a minimum of 50 characters'), DataRequired()])
+    response_expect = TextAreaField('What do you expect from our Community?', validators=[Length(min = 10, message = 'enter a minimum of 50 characters'), DataRequired()])
     submit = SubmitField('Submit')
     
 class LoginForm(FlaskForm):
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,29 @@
-from flask import render_template, request
-from app import app
+from crypt import methods
+from distutils.log import Log
+from app.models import Student
+from turtle import title
+from app.forms import RegistrationForm, LoginForm
+from flask import render_template, request, flash, redirect, url_for
+from app import app, db
+from app.forms import RegistrationForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        student = Student(reddit_username=form.reddit_username.data, course=form.course.data, \
+            response_join=form.response_join.data, response_goal=form.response_goal.data, \
+            response_pain=form.response_pain.data, response_expect=form.response_expect.data)
+        db.session.add(student)
+        db.session.commit()
+        flash('Registration is Successful!')
+    return render_template('registration.html', title='Registration Portal', form=form)
+
+
+
